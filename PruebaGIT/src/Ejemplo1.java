96028aecec46dc6b6da164d4036f881fa0c4e15d
Clara <101628024+claraSanPedro@users.noreply.github.com>
2022-03-17 18:48:09 +0100

    Update Ejemplo1.java
    Modificado desde GITHUB

--- PruebaGIT/src/Ejemplo1.java
+++ PruebaGIT/src/Ejemplo1.java
@@ -3,9 +3,10 @@ public class Ejemplo1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-System.out.println("Holaaa3");
+System.out.println("Holaaa34");
 System.out.println("Segunda línea");
 System.out.println("Tercera línea");
+		System.out.println("Línea desde GITHUB");
 	}
 
 }
