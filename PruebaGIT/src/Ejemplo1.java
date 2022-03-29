92a2cbe9cf447b60bf414172e36635cf71d7899e
grupo2 <grupo2@IES-Sampedro>
2022-03-29 16:24:24 +0200

    🔥 on fire

--- PruebaGIT/src/Ejemplo1.java
+++ PruebaGIT/src/Ejemplo1.java
@@ -3,9 +3,9 @@ public class Ejemplo1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-System.out.println("Holaaa");
-System.out.println("Segunda línea");
-System.out.println("Tercera línea");
+		System.out.println("Holaaa");
+		System.out.println("Segunda línea");
+		System.out.println("Tercera línea");
 	}
 
 }
